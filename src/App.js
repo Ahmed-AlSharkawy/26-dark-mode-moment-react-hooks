78594526ec92d32be57863e1b0246c0dc525549f
ahmed <fmt.a.sharkawy@gmail.com>
2021-08-03 11:48:59 +0000

    app finished

--- src/App.js
+++ src/App.js
@@ -2,8 +2,43 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+const getStorageTheme = () =>
+  localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light-theme'
+
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(getStorageTheme())
+
+  useEffect(() => {
+    document.documentElement.className = theme
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  return (
+    <main>
+      <nav>
+        <div className='nav-center'>
+          <h1>overreacted</h1>
+          <button
+            className='btn'
+            onClick={() => {
+              if (theme === 'light-theme') {
+                setTheme('dark-theme')
+              } else {
+                setTheme('light-theme')
+              }
+            }}
+          >
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className='articles'>
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />
+        })}
+      </section>
+    </main>
+  )
 }
 
 export default App
